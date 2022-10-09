4b0163965bc248304e96cd2788bc2134fe2d9bce
Paper010 <astondsouza369@gmail.com>
2022-10-09 13:34:04 +0530

    Added README.md and changed stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pong in Rust
+Classic Pong game recreated in rust using the macroquad framework.
+
+## Movements 
+- W : Move left paddle up
+- S: Move Left paddle down
+- Up: Move Right paddle up
+- Down: Move Right paddle down
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use macroquad::prelude::*;
 
+const PADDLE_SPEED: f32 = 10f32;
+const BALL_SPEED: f32 = 9f32;
 
-const PADDLE_SPEED:f32 = 10f32;
-const BALL_SPEED:f32 = 9f32;
-
-fn conf() -> Conf{
+fn conf() -> Conf {
     Conf {
         window_title: "Pong".to_string(),
         fullscreen: true,
         ..Default::default()
     }
 }
-
 
 enum GameState {
     Menu,
@@ -22,35 +20,34 @@ enum GameState {
 enum Position {
     Left,
     Right,
-    Center
+    Center,
 }
 
 struct Paddle {
-    rect:Rect,
+    rect: Rect,
 }
 
 struct Ball {
-    rect:Rect,
-    vel:Vec2,
+    rect: Rect,
+    vel: Vec2,
 }
 
-
 impl Paddle {
-    pub fn new(pos:Position) -> Paddle {
-        let pos:Vec2 = match pos {
-            Position::Left => vec2(100.,screen_height()/2.),
-            Position::Right => vec2(screen_width() / 2. + 550.,screen_height()/2.),
-            Position::Center => vec2(screen_width()/2.,screen_height()/2.),
-        }; 
+    pub fn new(pos: Position) -> Paddle {
+        let pos: Vec2 = match pos {
+            Position::Left => vec2(100., screen_height() / 2.),
+            Position::Right => vec2(screen_width() / 2. + 550., screen_height() / 2.),
+            Position::Center => vec2(screen_width() / 2., screen_height() / 2.),
+        };
 
         Paddle {
-            rect: Rect::new(pos.x,pos.y,30.,100.),    
+            rect: Rect::new(pos.x, pos.y, 30., 100.),
         }
     }
 
     pub fn update(&mut self) {
-        if self.rect.y > screen_height()-self.rect.h {
-            self.rect.y = screen_height()-self.rect.h;
+        if self.rect.y > screen_height() - self.rect.h {
+            self.rect.y = screen_height() - self.rect.h;
         };
 
         if self.rect.y < 0. {
@@ -59,110 +56,93 @@ impl Paddle {
     }
 
     pub fn draw(&self) {
-        draw_rectangle(self.rect.x,self.rect.y,self.rect.w,self.rect.h,WHITE);
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, WHITE);
     }
 }
 
 impl Ball {
-    pub fn new(pos:Position) -> Self {
-        let pos:Vec2 = match pos {
-            Position::Left => vec2(100.,screen_height()/2.),
-            Position::Right => vec2(screen_width() / 2. + 550.,screen_height()/2.),
-            Position::Center => vec2(screen_width()/2.,screen_height()/2.),
+    pub fn new(pos: Position) -> Self {
+        let pos: Vec2 = match pos {
+            Position::Left => vec2(100., screen_height() / 2.),
+            Position::Right => vec2(screen_width() / 2. + 550., screen_height() / 2.),
+            Position::Center => vec2(screen_width() / 2., screen_height() / 2.),
         };
 
         Self {
-            rect: Rect::new(pos.x,pos.y,40.,40.),    
-            vel: vec2(-1.,1.)
+            rect: Rect::new(pos.x, pos.y, 40., 40.),
+            vel: vec2(-1., 1.),
         }
     }
 
     pub fn update(&mut self) {
         self.rect.x += self.vel.x * BALL_SPEED;
         self.rect.y += self.vel.y * BALL_SPEED;
-        println!("{}",self.vel.y); 
-        if self.rect.y >screen_height()-self.rect.h{
-            println!("over board");
-            self.vel = vec2(self.vel.x,-self.vel.y*rand::gen_range(0.9,1.));
+
+        if self.rect.y > screen_height() - self.rect.h {
+            self.vel = vec2(self.vel.x, -self.vel.y * rand::gen_range(0.9, 1.));
         };
-        
+
         if self.rect.y < 0. {
-        println!("AAAAAAAAAAA");
-        self.vel = vec2(self.vel.x,1.*rand::gen_range(0.9,1.));
-        println!("{}",self.vel.y);
+            self.vel = vec2(self.vel.x, 1. * rand::gen_range(0.9, 1.));
         };
     }
 
     pub fn draw(&self) {
-        draw_rectangle(self.rect.x,self.rect.y,self.rect.w,self.rect.h,WHITE);
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, WHITE);
     }
 }
 
-fn handle_movement(player1:&mut Paddle, player2: &mut Paddle) {
+fn handle_movement(player1: &mut Paddle, player2: &mut Paddle) {
     if is_key_down(KeyCode::W) {
         player1.rect.y -= 2. * PADDLE_SPEED;
-    }else if is_key_down(KeyCode::S) {
+    } else if is_key_down(KeyCode::S) {
         player1.rect.y += 2. * PADDLE_SPEED;
-
     };
 
-
-    if is_key_down(KeyCode::Up){
+    if is_key_down(KeyCode::Up) {
         player2.rect.y -= 2. * PADDLE_SPEED;
-    }else if is_key_down(KeyCode::Down) {
+    } else if is_key_down(KeyCode::Down) {
         player2.rect.y += 2. * PADDLE_SPEED;
     };
 }
 
-fn handle_collision2(ball:&mut Ball, paddle: &Paddle) -> bool {
-    let mut vel = ball.vel;
-    
+fn handle_collision(ball: &mut Ball, paddle: &Paddle, side: Position) -> bool {
     let intersection = match ball.rect.intersect(paddle.rect) {
         Some(intersection) => intersection,
         None => return false,
     };
+
     let ball_center = ball.rect.point() + ball.rect.size() * 0.5f32;
     let paddle_center = paddle.rect.point() * paddle.rect.size() * 0.5f32;
     let to = paddle_center - ball_center;
     let to_signum = to.signum();
-    match intersection.w > intersection.h {
-        true => {
-            ball.rect.y -= to_signum.y * intersection.h;
-            ball.vel.y = -to_signum.y * ball.vel.y.abs();
-        }
-        false => {
-            ball.rect.x -= to_signum.x * intersection.w;
-            ball.vel.x = -to_signum.x * ball.vel.x.abs();
-        }
+
+    match side {
+        Position::Left => match intersection.w > intersection.h {
+            true => {
+                ball.rect.y += to_signum.y * intersection.h;
+                ball.vel.y = to_signum.y * ball.vel.y.abs();
+            }
+            false => {
+                ball.rect.x += to_signum.x * intersection.w;
+                ball.vel.x = to_signum.x * ball.vel.x.abs();
+            }
+        },
+        Position::Right => match intersection.w > intersection.h {
+            true => {
+                ball.rect.y -= to_signum.y * intersection.h;
+                ball.vel.y = -to_signum.y * ball.vel.y.abs();
+            }
+            false => {
+                ball.rect.x -= to_signum.x * intersection.w;
+                ball.vel.x = -to_signum.x * ball.vel.x.abs();
+            }
+        },
+        _ => {}
     }
+
     true
 }
-
-
-fn handle_collision(ball:&mut Ball, paddle: &Paddle) -> bool {
-    let mut vel = ball.vel;
-    
-    let intersection = match ball.rect.intersect(paddle.rect) {
-        Some(intersection) => intersection,
-        None => return false,
-    };
-    let ball_center = ball.rect.point() + ball.rect.size() * 0.5f32;
-    let paddle_center = paddle.rect.point() * paddle.rect.size() * 0.5f32;
-    let to = paddle_center - ball_center;
-    let to_signum = to.signum();
-    match intersection.w > intersection.h {
-        true => {
-            ball.rect.y += to_signum.y * intersection.h;
-            ball.vel.y = to_signum.y * ball.vel.y.abs();
-        }
-        false => {
-            ball.rect.x += to_signum.x * intersection.w;
-            ball.vel.x = to_signum.x * ball.vel.x.abs();
-        }
-    }
-    true
-}
-
 
 #[macroquad::main(conf)]
 async fn main() {
@@ -171,80 +151,99 @@ async fn main() {
     let mut ball = Ball::new(Position::Center);
 
     let mut game_state = GameState::Menu;
-    let mut winner:String = "No one".to_string();
+    let mut winner: String = "No one".to_string();
 
     loop {
         clear_background(BLACK);
 
         match game_state {
-        GameState::Menu=> {
-        draw_text_ex("Pong!",screen_width()/2.-190.,screen_height()/2.,TextParams {
-            font_size: 80,
-            ..Default::default()
-        });
-        
-        draw_text_ex("Press Space to start!",screen_width()/2.-290.,screen_height()/2. + 50.,TextParams {
-            font_size: 70,
-            ..Default::default()
-        });
-        
-        draw_text_ex("Press Esc to exit",screen_width()/2.-290.,screen_height()/2.+100.,TextParams {
-            font_size: 60,
-            ..Default::default()
-        });       
-        
-        if is_key_pressed(KeyCode::Space) {
-            game_state = GameState::Playing;
-        };
-        
-        },
-        GameState::Playing => {
-        ball.draw();
-        ball.update();
+            GameState::Menu => {
+                draw_text_ex(
+                    "Pong!",
+                    screen_width() / 2. - 190.,
+                    screen_height() / 2.,
+                    TextParams {
+                        font_size: 80,
+                        ..Default::default()
+                    },
+                );
 
-        if ball.rect.x > screen_width() - ball.rect.w {
-            winner = "Player 1".to_string();
-            game_state = GameState::Over;
-            
-        }else if ball.rect.x < 0. {
-            winner = "Player 2".to_string();
-            game_state = GameState::Over;
-        };
+                draw_text_ex(
+                    "Press Space to start!",
+                    screen_width() / 2. - 290.,
+                    screen_height() / 2. + 50.,
+                    TextParams {
+                        font_size: 70,
+                        ..Default::default()
+                    },
+                );
 
-        handle_movement(&mut player1,&mut player2);
-        player1.update();
-        player2.update();
-       
-        handle_collision(&mut ball, &player1);
-        handle_collision2(&mut ball, &player2);
-        player1.draw();
-        player2.draw();
+                draw_text_ex(
+                    "Press Esc to exit",
+                    screen_width() / 2. - 290.,
+                    screen_height() / 2. + 100.,
+                    TextParams {
+                        font_size: 60,
+                        ..Default::default()
+                    },
+                );
 
-        },
-        GameState::Over => {
-         
-        draw_text_ex(&String::from(format!("{winner} Wins!")),screen_width()/2.-290.,screen_height()/2.,TextParams {
-            font_size: 70,
-            ..Default::default()
-        });
-        draw_text_ex("Press Space to restart",screen_width()/2.-290.,screen_height()/2. + 50.,TextParams {
-            font_size: 70,
-            ..Default::default()
-        });
-      
-         if is_key_pressed(KeyCode::Space){
-             ball = Ball::new(Position::Center);
-             player1 = Paddle::new(Position::Left);
-             player2 = Paddle::new(Position::Right);
-            game_state = GameState::Playing;
-        };
-        },
+                if is_key_pressed(KeyCode::Space) {
+                    game_state = GameState::Playing;
+                };
+            }
+            GameState::Playing => {
+                ball.draw();
+                ball.update();
 
-       
+                if ball.rect.x > screen_width() - ball.rect.w {
+                    winner = "Player 1".to_string();
+                    game_state = GameState::Over;
+                } else if ball.rect.x < 0. {
+                    winner = "Player 2".to_string();
+                    game_state = GameState::Over;
+                };
+
+                handle_movement(&mut player1, &mut player2);
+                player1.update();
+                player2.update();
+
+                handle_collision(&mut ball, &player1, Position::Left);
+                handle_collision(&mut ball, &player2, Position::Right);
+
+                player1.draw();
+                player2.draw();
+            }
+            GameState::Over => {
+                draw_text_ex(
+                    &String::from(format!("{winner} Wins!")),
+                    screen_width() / 2. - 290.,
+                    screen_height() / 2.,
+                    TextParams {
+                        font_size: 70,
+                        ..Default::default()
+                    },
+                );
+                draw_text_ex(
+                    "Press Space to restart",
+                    screen_width() / 2. - 290.,
+                    screen_height() / 2. + 50.,
+                    TextParams {
+                        font_size: 70,
+                        ..Default::default()
+                    },
+                );
+
+                if is_key_pressed(KeyCode::Space) {
+                    ball = Ball::new(Position::Center);
+                    player1 = Paddle::new(Position::Left);
+                    player2 = Paddle::new(Position::Right);
+                    game_state = GameState::Playing;
+                };
+            }
         }
-        
 
-        // quit on esc 
+        // quit on esc
         if is_key_pressed(KeyCode::Escape) {
             return;
         };
